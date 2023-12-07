2e131a9a8b368b6c3fdc8b65906caabf14bd5faf
riya-rjha <riyaranjanjha2006@gmail.com>
2023-12-07 22:06:22 +0530

    Update a book

--- Backend/index.js
+++ Backend/index.js
@@ -26,3 +26,80 @@ app.listen(PORT, () => {
 })
 
 
+//Route to save a new book
+//Postman API to get data
+app.post('/books', async (req, res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: 'Send all the required details'
+            });
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        };
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+        //uses postman to create and send https requests
+    } catch (error) {
+        console.log(`Error message : ${error.message}`);
+        return res.status(500).send({ message: error.message });
+    }
+});
+
+//route to get all books from database
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find({});
+        return res.status(201).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(`Error message : ${error.message}`);
+        return res.status(500).send({ message: error.message });
+    }
+})
+
+//get one book from an id
+app.get('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const book = await Book.findById(id);
+        return res.status(201).json(book);
+
+    } catch (error) {
+        console.log(`Error message : ${error.message}`);
+        return res.status(500).send({message : error.message});
+    }
+})
+
+//update a book from an id
+app.put('/books/:id', async(req, res)=>{
+    try{
+        if(
+            !req.body.title ||
+            !req.body.author || 
+            !req.body.publishYear
+        ){
+            return res.status(500).send({message : 'Send all the required details!'});
+        }
+
+        const {id} = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+        if(!result){
+            return res.status(500).json({message : 'Book not found!'})
+        }
+        return res.status(200).send({message : 'Book updated successfully'});
+    }
+    catch(err){
+        console.log(`Error message : ${err.message}`);
+        return res.status(500).send({message : err.message});
+    }
+});
