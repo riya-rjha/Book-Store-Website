372d5e084ce51fa67e89c169bfa0921d5ee3663d
riya-rjha <riyaranjanjha2006@gmail.com>
2023-12-10 11:58:31 +0530

    Add book editing functionality

--- Frontend/src/Pages/EditBook.jsx
+++ Frontend/src/Pages/EditBook.jsx
@@ -1,8 +1,99 @@
 import React from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import Spinner from '../Components/Spinner.jsx'
+import BackButton from '../Components/BackButton.jsx'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditBook = () => {
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {id} = useParams();
+
+  const navigate = useNavigate();
+
+  //Get book of specified ID
+  useEffect(() => {
+    setIsLoading(true);
+    axios.
+      get(`http://localhost:9999/books/${id}`)
+      .then((response)=>{
+        setTitle(response.data.title);
+        setAuthor(response.data.author);
+        setPublishYear(response.data.publishYear);
+        setIsLoading(false);
+      })
+      .catch((err)=>{
+        console.log(err.message);
+        setIsLoading(false);
+      })
+  }, []);
+
+  //Post book of specified edits in ID
+  const handleEdit = () => {
+    const data = {
+      title, author, publishYear
+    }
+    setIsLoading(true);
+    axios
+      .put(`http://localhost:9999/books/${id}`, data)
+      .then(() => {
+        setIsLoading(false);
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <div>EditBook</div>
+    <div >
+      <BackButton />
+      <h1 className='text-red-950 m-8 text-2xl font-bold font-serif'>Create book : </h1>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="border border-green-500 flex justify-center flex-col mx-40 mt-10 p-15 rounded-lg  ">
+          <div className="m-2 flex flex-col">
+            <label className='font-serif text-green-800 text-2xl font-semibold'>Title : </label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className='border border-green-900 outline-none p-1.5 rounded-md my-2 font-serif'
+            />
+          </div>
+          <div className="m-2 flex flex-col">
+            <label className='font-serif text-green-800 text-2xl font-semibold'>Author : </label>
+            <input
+              type="text"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className='border border-green-900 outline-none p-1.5 rounded-md my-2 font-serif'
+            />
+          </div>
+          <div className="m-2 flex flex-col">
+            <label className='font-serif text-green-800 text-2xl font-semibold'>Publish Year : </label>
+            <input
+              type="text"
+              value={publishYear}
+              onChange={(e) => setPublishYear(e.target.value)}
+              className='border border-green-900 outline-none p-1.5 rounded-md my-2 font-serif'
+            />
+          </div>
+          <button
+            className=' p-2 border border-green-900 text-22xl m-2 rounded-md hover:bg-green-200 font-bold text-green-950'
+            onClick={handleEdit}
+          >Submit</button>
+
+        </div>
+      )}
+    </div>
   )
 }
 
