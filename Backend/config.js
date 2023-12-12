a6a79f14b8a07ca9c51c8ceb04bbff98a3606d31
riya-rjha <riyaranjanjha2006@gmail.com>
2023-12-12 17:36:31 +0530

    Remove config.js from .gitignore

---
+++ Backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 9999;
+
+export const mongoDBURL = 'mongodb+srv://botchat137:KWaH13GpRHlbjQV5@bookstorewebsite.oxuikm4.mongodb.net/books-collection?retryWrites=true&w=majority';
+
