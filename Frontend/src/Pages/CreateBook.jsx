4bc9e17330a43d8c35febd8e60ee353e27091c9d
Manisangh Sharma <manisanghsharma@gmail.com>
2023-12-12 18:14:04 +0530

    Fix formatting and add error handling in CreateBook and EditBook components

--- Frontend/src/Pages/CreateBook.jsx
+++ Frontend/src/Pages/CreateBook.jsx
@@ -22,7 +22,7 @@ const CreateBook = () => {
     }
     setIsLoading(true);
     axios
-      .post('https://book-store-snzt.onrender.combooks', data)
+      .post('https://book-store-snzt.onrender.com/books', data)
       .then(() => {
         setIsLoading(false);
         enqueueSnackbar('Book created successfully!', { variant: 'success' });
@@ -36,49 +36,58 @@ const CreateBook = () => {
   };
 
   return (
-    <div >
-      <BackButton />
-      <h1 className='text-red-950 m-8 text-2xl font-bold '>Create book : </h1>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-          <div className="m-2 flex flex-col">
-            <label className=' text-green-800 text-2xl font-semibold'>Title : </label>
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className='border border-green-900 outline-none p-1.5 rounded-md my-2 '
-            />
-          </div>
-          <div className="m-2 flex flex-col">
-            <label className=' text-green-800 text-2xl font-semibold'>Author : </label>
-            <input
-              type="text"
-              value={author}
-              onChange={(e) => setAuthor(e.target.value)}
-              className='border border-green-900 outline-none p-1.5 rounded-md my-2 '
-            />
-          </div>
-          <div className="m-2 flex flex-col">
-            <label className=' text-green-800 text-2xl font-semibold'>Publish Year : </label>
-            <input
-              type="text"
-              value={publishYear}
-              onChange={(e) => setPublishYear(e.target.value)}
-              className='border border-green-900 outline-none p-1.5 rounded-md my-2 '
-            />
-          </div>
-          <button
-            className=' p-2 border border-green-900 text-22xl m-2 rounded-md hover:bg-green-200 font-bold text-green-950'
-            onClick={handleSubmit}
-          >Submit</button>
-
-        </div>
-      )}
-    </div>
-  )
+		<div>
+			<BackButton />
+			<h1 className='text-red-950 m-8 text-2xl font-bold '>Create book : </h1>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<div className='w-full flex justify-center items-center'>
+					<div className='flex flex-col border-2 border-sky-400 rounded-xl w-[450px] min-w-[150px] p-4 mx-5'>
+						<div className='m-2 flex flex-col'>
+							<label className=' text-green-800 text-2xl font-semibold'>
+								Title :
+							</label>
+							<input
+								type='text'
+								value={title}
+								onChange={(e) => setTitle(e.target.value)}
+								className='border border-green-900 outline-none p-1.5 rounded-md my-2 '
+							/>
+						</div>
+						<div className='m-2 flex flex-col'>
+							<label className=' text-green-800 text-2xl font-semibold'>
+								Author :{" "}
+							</label>
+							<input
+								type='text'
+								value={author}
+								onChange={(e) => setAuthor(e.target.value)}
+								className='border border-green-900 outline-none p-1.5 rounded-md my-2 '
+							/>
+						</div>
+						<div className='m-2 flex flex-col'>
+							<label className=' text-green-800 text-2xl font-semibold'>
+								Publish Year :{" "}
+							</label>
+							<input
+								type='text'
+								value={publishYear}
+								onChange={(e) => setPublishYear(e.target.value)}
+								className='border border-green-900 outline-none p-1.5 rounded-md my-2 '
+							/>
+						</div>
+						<button
+							className=' p-2 border border-green-900 text-22xl m-2 rounded-md hover:bg-green-200 font-bold text-green-950'
+							onClick={handleSubmit}
+						>
+							Submit
+						</button>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default CreateBook
